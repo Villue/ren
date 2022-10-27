e5b69d601d6843417db2c19fc90b53d894ca01cc
Villue <1lebolga1@gmail.com>
2022-10-27 18:29:28 +0300

    что-нибудь

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance;
+
+    public static int Points { get; private set; }
+
+    public static bool GameStarted { get; private set; }
+
+    [SerializeField]
+    private TextMeshProUGUI gameResult;
+    [SerializeField]
+    private TextMeshProUGUI pointsText;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+        gameResult.text = "";
+
+        SetPoints(0);
+        GameStarted = true;
+
+        Field.Instance.GenerateField();
+    }
+
+    public void Win()
+    {
+        GameStarted = false;
+        gameResult.text = "You Win!";
+    }
+
+    public void Lose()
+    {
+        GameStarted = false;
+        gameResult.text = "You Lose!";
+    }
+
+    public void AddPoints(int points)
+    {
+        SetPoints(Points + points);
+    }
+
+    private void SetPoints(int points)
+    {
+        Points = points;
+        pointsText.text = Points.ToString();
+    }
+}
